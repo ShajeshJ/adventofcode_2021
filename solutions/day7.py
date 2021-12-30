2e0c344040639373af7665c5908a726dca14571b
SHAJESH JEGATHEESWARAN <shajesh.14@gmail.com>
2021-12-30 18:57:49 -0500

    Update day 7 with more optimized solution

--- solutions/day7.py
+++ solutions/day7.py
@@ -1,5 +1,56 @@
+import math
 from libraries.questions import get_question_input
 
+
+def part_1():
+    data = get_question_input(7)
+    crab_positions = [int(p) for p in next(data).split(",")]
+
+    # The ideal position is just the median of all the positions in this case
+    num_crabs = len(crab_positions)
+    crab_positions.sort()
+    if num_crabs % 2 != 0:
+        # For odd length lists, the median is the middle number
+        ideal_position = crab_positions[num_crabs // 2]
+    else:
+        # For even length lists, the median is the average of the middle numbers
+        ideal_position = (
+            crab_positions[num_crabs // 2] + crab_positions[num_crabs // 2 - 1]
+        ) // 2
+
+    ideal_usage = sum(abs(p - ideal_position) for p in crab_positions)
+    print(ideal_usage)
+
+
+def part_2():
+    data = get_question_input(7)
+    crab_positions = [int(p) for p in next(data).split(",")]
+
+    def _calculate_usage(t: int):
+        """Calculates usage using sum of arithmetic series formula, (n+1)*n/2"""
+        return int(sum((abs(p - t) + 1) * abs(p - t) / 2 for p in crab_positions))
+
+    # The ideal position is just the mean of all positions in this case
+    ideal_position = sum(crab_positions) / len(crab_positions)
+
+    if ideal_position.is_integer():
+        ideal_position = int(ideal_position)
+        ideal_usage = _calculate_usage(ideal_position)
+    else:
+        # Target cannot be decimal so we'll test both integers around the target
+        ideal_position = int(ideal_position)
+        ideal_usage = _calculate_usage(ideal_position)
+
+        contest_usage = _calculate_usage(ideal_position + 1)
+        if contest_usage < ideal_usage:
+            ideal_position += 1
+            ideal_usage = contest_usage
+
+    print(ideal_usage)
+
+
+"""
+Original attempts at parts 1&2:
 
 def part_1():
     data = get_question_input(7)
@@ -42,3 +93,4 @@ def part_2():
             ideal_usage = candidate_usage
 
     print(ideal_usage)
+ """
