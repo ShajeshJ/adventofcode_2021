d631b6d845d5eb929d61a58d415e1788e0060e03
SHAJESH JEGATHEESWARAN <shajesh.14@gmail.com>
2021-12-30 05:25:31 -0500

    Move all variables in main.py out of the global namespace

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 from libraries.questions import get_answer_module, run_answer
 import argparse
 
-parser = argparse.ArgumentParser(description="Run Advent of Code 2021")
-parser.add_argument(
-    "--day",
-    help="The day of code to run; will be prompted if not specified",
-    type=int,
-    default=None,
-)
-parser.add_argument(
-    "--part",
-    help="Which part in the day to run; will be prompted if not specified",
-    type=int,
-    default=None,
-)
 
-args = parser.parse_args()
+def main():
 
-if __name__ == "__main__":
+    # Configure command line parser
+
+    parser = argparse.ArgumentParser(description="Run Advent of Code 2021")
+    parser.add_argument(
+        "--day",
+        help="The day of code to run; will be prompted if not specified",
+        type=int,
+        default=None,
+    )
+    parser.add_argument(
+        "--part",
+        help="Which part in the day to run; will be prompted if not specified",
+        type=int,
+        default=None,
+    )
+    args = parser.parse_args()
+
+    # Code start
+
     day = args.day or input("Enter the day # to run code for: ")
     answer_mod = get_answer_module(day)
 
     part = args.part or input("Enter the question part # to execute: ")
     run_answer(answer_mod, part)
+
+
+if __name__ == "__main__":
+    main()
