711119cd8a8cbb2a27e7352ea8b7493192250c26
SHAJESH JEGATHEESWARAN <shajesh.14@gmail.com>
2022-01-07 01:58:11 -0500

    Complete day 10 part 2

--- solutions/day10.py
+++ solutions/day10.py
@@ -7,15 +7,14 @@ __CHUNK_CHARS = {
     "<": ">",
 }
 
-__ERROR_SCORE = {
-    ")": 3,
-    "]": 57,
-    "}": 1197,
-    ">": 25137,
-}
-
 
 def part_1():
+    score_lookup = {
+        ")": 3,
+        "]": 57,
+        "}": 1197,
+        ">": 25137,
+    }
     error_score = 0
 
     for seq in get_question_input(10):
@@ -28,6 +27,45 @@ def part_1():
 
             # Corrupted chunk found
             if __CHUNK_CHARS[parse_stack.pop()] != c:
-                error_score += __ERROR_SCORE[c]
+                error_score += score_lookup[c]
                 break
+
     print(error_score)
+
+
+def part_2():
+    score_lookup = {
+        ")": 1,
+        "]": 2,
+        "}": 3,
+        ">": 4,
+    }
+    incomplete_scores = []
+
+    for seq in get_question_input(10):
+        parse_stack = []
+        is_corrupted = False
+
+        for c in seq:
+            # Start of new chunk
+            if c in __CHUNK_CHARS.keys():
+                parse_stack.append(c)
+                continue
+
+            # Corrupted chunk found
+            if __CHUNK_CHARS[parse_stack.pop()] != c:
+                is_corrupted = True
+                break
+
+        if not is_corrupted:
+            score = 0
+            while len(parse_stack) > 0:
+                score *= 5
+                score += score_lookup[__CHUNK_CHARS[parse_stack.pop()]]
+
+            # Score of 0 is a complete and correct chunk
+            if score > 0:
+                incomplete_scores.append(score)
+
+    incomplete_scores.sort()
+    print(incomplete_scores[len(incomplete_scores) // 2])
