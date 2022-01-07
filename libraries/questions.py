221aeaa78592d09b60d7dc05b312fa4cb27a4da5
SHAJESH JEGATHEESWARAN <shajesh.14@gmail.com>
2022-01-07 03:09:46 -0500

    Make meta text gray

--- libraries/questions.py
+++ libraries/questions.py
@@ -5,7 +5,7 @@ from types import ModuleType
 import importlib
 import inspect
 import time
-from libraries.print_util import make_red
+from libraries.print_util import make_gray, make_red
 
 
 def get_question_input(day: int) -> Generator[str, None, None]:
@@ -34,7 +34,12 @@ def run_answer(answer_module: ModuleType, part: int):
     except StopIteration:
         sys.exit(make_red(f"No function `part_{part}` defined in target module"))
 
+    print(make_gray("-" * 6 + " SOLUTION OUTPUT " + "-" * 6 + "\033[0m"))
+
     start = time.perf_counter()
     target_func()
     end = time.perf_counter()
-    print(f"Execution time: {(end - start)*1000:3f} ms")
+
+    print(make_gray("-" * 29))
+    print(make_gray(f"Execution time: {(end - start)*1000:3f} ms"))
+    print(make_gray("-" * 29))
