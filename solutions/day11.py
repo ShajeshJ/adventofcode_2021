0879d99b61ae2f7ad82aebf2a32c34dae654b02a
SHAJESH JEGATHEESWARAN <shajesh.14@gmail.com>
2022-01-08 01:32:44 -0500

    Complete day 11 part 2, and refactor code

--- solutions/day11.py
+++ solutions/day11.py
@@ -29,6 +29,42 @@ def __get_adjacent(x: int, y: int, map: list[list[int]]):
     return adjacent_indices
 
 
+def __simulate_step(octo_map: list[list[int]]):
+    """Given an octopus map, will simulate the next step of flashes.
+
+    Returns the octopus map with the new flash values, as well as
+    a list of coordinates for all the octopuses who flashed during the step.
+    """
+    # Increment flash charges of all octopus
+    octo_map = __update_map(lambda x: x + 1, octo_map)
+
+    # Get seed flashers
+    flashers: set[tuple[int, int]] = set()
+    for x, row in enumerate(octo_map):
+        for y, flash_amt in enumerate(row):
+            if flash_amt > 9:
+                flashers.add((x, y))
+
+    # Process flashes and chain flashes
+    flashed: set[tuple[int, int]] = set()
+    while len(flashers) > 0:
+        flasher = flashers.pop()
+        flashed.add(flasher)
+
+        # Don't double count adjacents that already flashed
+        adjacent = set(__get_adjacent(*flasher, octo_map)) - flashed
+        for a_x, a_y in adjacent:
+            octo_map[a_x][a_y] += 1
+            if octo_map[a_x][a_y] > 9:
+                flashers.add((a_x, a_y))
+
+    # Reset flash charge for the octopuses that flashed
+    for x, y in flashed:
+        octo_map[x][y] = 0
+
+    return octo_map, flashed
+
+
 def part_1():
     data = get_question_input(11)
     octopuses: list[list[int]] = [[int(h) for h in next(data)]]
@@ -43,32 +79,28 @@ def part_1():
 
     # Simulating 100 steps
     for _ in range(100):
-        # Increment flash counts
-        octopuses = __update_map(lambda x: x + 1, octopuses)
-
-        # Get seed flashers
-        flashers: set[tuple[int, int]] = set()
-        for x, row in enumerate(octopuses):
-            for y, flash_amt in enumerate(row):
-                if flash_amt > 9:
-                    flashers.add((x, y))
-
-        # Process flashes and chain flashes
-        flashed: set[tuple[int, int]] = set()
-        while len(flashers) > 0:
-            flasher = flashers.pop()
-            flashed.add(flasher)
-            num_flashes += 1
-
-            # Don't double count adjacents that flashed
-            adjacent = set(__get_adjacent(*flasher, octopuses)) - flashed
-            for a_x, a_y in adjacent:
-                octopuses[a_x][a_y] += 1
-                if octopuses[a_x][a_y] > 9:
-                    flashers.add((a_x, a_y))
-
-        # Reset all octopuses that flashed
-        for x, y in flashed:
-            octopuses[x][y] = 0
+        octopuses, flashed_octopuses = __simulate_step(octopuses)
+        num_flashes += len(flashed_octopuses)
 
     print(num_flashes)
+
+
+def part_2():
+    data = get_question_input(11)
+    octopuses: list[list[int]] = [[int(h) for h in next(data)]]
+    map_w = len(octopuses[0])
+
+    for line in data:
+        if len(line) != map_w:
+            sys.exit(make_red("Octopus map data does not have constant width"))
+        octopuses.append([int(h) for h in line])
+
+    num_octopuses = map_w * len(octopuses)
+    flashed_octopuses: set[tuple[int, int]] = set()
+    step = 0
+
+    while len(flashed_octopuses) != num_octopuses:
+        step += 1
+        octopuses, flashed_octopuses = __simulate_step(octopuses)
+
+    print(step)
