38b14b505aed596151474daf596b49cd2c7a1cf1
SHAJESH JEGATHEESWARAN <shajesh.14@gmail.com>
2022-04-23 04:04:24 -0400

    Completed day 18 part 1

---
+++ solutions/day18.py
@@ -0,0 +1,229 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from math import ceil, floor
+from turtle import left
+from typing import Optional
+from libraries.questions import get_question_input
+
+
+_SPLIT_THRESHOLD = 10
+
+
+def _tokenize(snailfish_str: str) -> list[str | int]:
+    """Converts `snailfish_str` into a list of tokens"""
+    tokens = []
+    token_builder = ""
+
+    def _flush_builder():
+        nonlocal token_builder
+        if token_builder.isdigit():
+            tokens.append(int(token_builder))
+            token_builder = ""
+
+    for c in snailfish_str:
+        if c in ["[", ",", "]"]:
+            _flush_builder()
+            tokens.append(c)
+        elif c.isdigit():
+            token_builder += c
+        else:
+            raise ValueError(f"Failed to tokenize: Invalid character '{c}' found")
+
+    _flush_builder()
+    return tokens
+
+
+def _get_exploding(
+    snailfish_num: SnailfishNumber, depth: int = 0
+) -> SnailfishNumber | None:
+    """Returns the SnailfishNumber object that should explode, if it exists"""
+    if depth == 4:
+        return snailfish_num
+
+    for operand in (snailfish_num.left, snailfish_num.right):
+        if not isinstance(operand, type(snailfish_num)):
+            continue
+        if exploder := _get_exploding(operand, depth + 1):
+            return exploder
+
+    return None
+
+
+def _get_split_container(snailfish_num: SnailfishNumber) -> SnailfishNumber | None:
+    """Returns the SnailfishNumber object containing
+    a number which should split, if it exists
+    """
+    for operand in (snailfish_num.left, snailfish_num.right):
+        if isinstance(operand, int) and operand >= _SPLIT_THRESHOLD:
+            return snailfish_num
+        if not isinstance(operand, type(snailfish_num)):
+            continue
+        if split_container := _get_split_container(operand):
+            return split_container
+
+    return None
+
+
+@dataclass
+class SnailfishNumber:
+    left: int | SnailfishNumber
+    right: int | SnailfishNumber
+    parent: Optional[SnailfishNumber] = None
+
+    def explode(self: SnailfishNumber):
+        """Explodes self"""
+        if not isinstance(self.left, int) or not isinstance(self.right, int):
+            raise ArithmeticError("Cannot reduce snailfish # with depth > 4")
+
+        self.add_to_left(self.left)
+        self.add_to_right(self.right)
+
+        if self.parent.left is self:
+            self.parent.left = 0
+        elif self.parent.right is self:
+            self.parent.right = 0
+        else:
+            raise Exception(f"No explosion available in {self}")
+        self.parent = None  # To avoid memory leaking
+
+    def split_entry(self: SnailfishNumber):
+        """Splits the left most entry >= _SPLIT_THRESHOLD within self"""
+        cls = type(self)
+        if isinstance(self.left, int) and self.left >= _SPLIT_THRESHOLD:
+            self.left = cls(floor(self.left / 2), ceil(self.left / 2))
+        elif isinstance(self.right, int) and self.right >= _SPLIT_THRESHOLD:
+            self.right = cls(floor(self.right / 2), ceil(self.right / 2))
+        else:
+            raise Exception(f"No split available in {self}")
+        self.update_refs_in_children()
+
+    def reduce(self: SnailfishNumber):
+        """Reduces self"""
+        while True:
+            if exploder := _get_exploding(self):
+                exploder.explode()
+                continue
+            if split_container := _get_split_container(self):
+                split_container.split_entry()
+                continue
+            break
+
+    def magnitude(self: SnailfishNumber) -> int:
+        """Returns the magnitude of self"""
+        left_magnitude = 3 * (
+            self.left if isinstance(self.left, int) else self.left.magnitude()
+        )
+        right_magnitude = 2 * (
+            self.right if isinstance(self.right, int) else self.right.magnitude()
+        )
+        return left_magnitude + right_magnitude
+
+    def add_to_left(self: SnailfishNumber, value: int):
+        """Adds `value` to the next operand found traversing left"""
+        if self.parent is None:
+            # Structure: [self, ...]
+            pass
+        elif self.parent.left is self:
+            # Structure: parent[self, ...]
+            self.parent.add_to_left(value)
+        elif isinstance(self.parent.left, int):
+            # Structure: parent[int, self]
+            self.parent.left += value
+        else:
+            # Structure: parent[left[...,int], self]
+            next_left = self.parent.left
+            while not isinstance(next_left.right, int):
+                next_left = next_left.right
+            next_left.right += value
+
+    def add_to_right(self: SnailfishNumber, value: int):
+        """Adds `value` to the next operand found traversing right"""
+        if self.parent is None:
+            # Structure: [..., self]
+            pass
+        elif self.parent.right is self:
+            # Structure: parent[..., self]
+            self.parent.add_to_right(value)
+        elif isinstance(self.parent.right, int):
+            # Structure: parent[self, int]
+            self.parent.right += value
+        else:
+            # Structure: parent[self, right[int, ...]]
+            next_right = self.parent.right
+            while not isinstance(next_right.left, int):
+                next_right = next_right.left
+            next_right.left += value
+
+    def update_refs_in_children(self: SnailfishNumber):
+        """Updates operand's parent ref to self, where applicable"""
+        if isinstance(self.left, type(self)):
+            self.left.parent = self
+        if isinstance(self.right, type(self)):
+            self.right.parent = self
+
+    @classmethod
+    def from_tokens(
+        cls: type[SnailfishNumber], tokens: list[str | int]
+    ) -> SnailfishNumber:
+        """Uses the given `tokens` to build a `SnailfishNumber` object"""
+
+        def _pop_char(c: str):
+            t = tokens.pop(0)
+            if t != c:
+                raise ValueError(f"Expected `{c}` token but found `{t}` instead")
+
+        def _parse_expression() -> SnailfishNumber | int:
+            if tokens[0] == "[":
+                return cls.from_tokens(tokens)
+            elif isinstance(tokens[0], int):
+                return tokens.pop(0)
+            else:
+                raise ValueError(
+                    f"Expected `[` or integer token but found `{tokens[0]}` instead"
+                )
+
+        # Parse tokens
+        _pop_char("[")
+        left = _parse_expression()
+        _pop_char(",")
+        right = _parse_expression()
+        _pop_char("]")
+
+        obj = cls(left, right)
+        obj.update_refs_in_children()
+        return obj
+
+    @classmethod
+    def load(cls: type[SnailfishNumber], snailfish_str: str) -> SnailfishNumber:
+        """Parses and creates a `SnailfishNumber` object from `snailfish_str`"""
+        tokens = _tokenize(snailfish_str)
+        orig_len = len(tokens)
+        try:
+            return cls.from_tokens(tokens)
+        except Exception as e:
+            raise ValueError(
+                f"The snailfish_str {snailfish_str} has an invalid "
+                f"token at around position {orig_len-len(tokens)}"
+            ) from e
+
+    def __add__(self: SnailfishNumber, other: SnailfishNumber) -> SnailfishNumber:
+        if not isinstance(other, type(self)):
+            raise TypeError(f"Cannot add object of type {type(other)} to {type(self)}")
+
+        sum = SnailfishNumber(self, other)
+        sum.update_refs_in_children()
+        sum.reduce()
+        return sum
+
+    def __repr__(self) -> str:
+        return f"[{self.left},{self.right}]"
+
+
+def part_1():
+    data = get_question_input(18)
+    snailfish_sum = sum(
+        (SnailfishNumber.load(snailfish_str) for snailfish_str in data),
+        start=SnailfishNumber.load(next(data)),
+    )
+    print(snailfish_sum)
+    print(snailfish_sum.magnitude())
